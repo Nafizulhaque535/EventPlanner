5778c1ef5295a7b79a9f23c772f51910d2def4fd
Mirza <113276258+Mirza-Abbas-Uddin@users.noreply.github.com>
2023-03-15 23:55:54 +0600

    Create database_connection.php
    Create User Profile:- Open xampp->go to phpMyAdmin->User accounts->Add user account->Input the followings->Active Database for user account(Create database with same name and grant all privileges, Grant all privileges on wildcard name (username\_%) -> Active Global privileges(check all) ->go.

---
+++ database_connection.php
@@ -0,0 +1,11 @@
+<?php
+//creaste user profile in phpMyAdmin using this
+$servername = "localhost";
+$username = "eventplanner";
+$password = "eventplanner123";
+$database = "eventplanner";
+$conn = new mysqli($servername, $username, $password, $database);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
